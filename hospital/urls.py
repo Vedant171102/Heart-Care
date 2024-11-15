e437187a5ed567059234d2f0fcba947b97a78ddb
Vedant171102 <vedantmahamulkar@gmail.com>
2024-11-15 18:36:56 +0530

    new changes

---
+++ hospital/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='index'),
+    path('services/', views.ServiceListView.as_view(), name="services"),
+    path('services/<int:pk>/', views.ServiceDetailView.as_view(),
+         name="service_details"),
+    path('doctors/', views.DoctorListView.as_view(), name="doctors"),
+    path('doctors/<int:pk>/', views.DoctorDetailView.as_view(),
+         name="doctor_details"),
+    path('faqs/', views.FaqListView.as_view(), name="faqs"),
+    path('gallery/', views.GalleryListView.as_view(), name="gallery"),
+    path('contact/', views.ContactView.as_view(), name="contact")
+]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
